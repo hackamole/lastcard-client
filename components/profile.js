28f376d725737da5b2f5efd0c9ee7370d769b09e
Miguel Carvalho <miguel@brpx.com>
2019-03-21 18:53:14 +0000

    Add DOM structure for profile component

--- components/profile.js
+++ components/profile.js
@@ -4,10 +4,26 @@ import styled from 'styled-components'
 const Profile = styled.section`
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
 export default () => (
   <Profile>
-    profile
+    <div>Avatar</div>
+    <h1>Name</h1>
+    <h3>title</h3>
+    <p>
+      <span>email</span>
+      <span>phone</span>
+    </p>
+    <section>
+      <h5>add to:</h5>
+      <ul>
+        <li>v-card</li>
+        <li>linkedin</li>
+        <li>github</li>
+      </ul>
+    </section>
   </Profile>
 )
--- components/takeover.js
+++ components/takeover.js
@@ -4,10 +4,15 @@ import styled from 'styled-components'
 const Takeover = styled.section`
   height: 64px;
   width: 100%;
+  background-color: #E8E8E8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #797979;
 `
 
 export default () => (
   <Takeover>
-    Takeover
+    take over this card
   </Takeover>
 )
