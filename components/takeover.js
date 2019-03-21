b7db52b9910beebbd4fdb427540e7a40847959c6
Miguel Carvalho <miguel@brpx.com>
2019-03-21 23:22:55 +0000

    Add login buttons and takeover message

--- components/takeover.js
+++ components/takeover.js
@@ -11,17 +11,55 @@ const TakeoverWrapper = styled.section`
   justify-content: center;
   align-items: flex-start;
   color: #797979;
-  transition: height 500ms cubic-bezier(1, 0, 0, 1);
+  transition: height 500ms cubic-bezier(0.19, 1, 0.22, 1);
   
   &.isExpanded {
     height: 100%;
   }
   
-  button {
+  button.toggle {
+    z-index: 1;
+    position: fixed;
+    bottom: 0;
     width: 100%;
-    height: 100%;
+    height: 64px;
     margin: 0;
     padding: 0;
+    background-color: #E8E8E8;
+    color: #797979;
+    outline: none;
+  }
+  
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 6rem 4rem 4rem;
+    
+    p {
+      font-size: 1.5rem;
+      padding: 4rem 0;
+    }
+    section {
+      width: 100%;
+      
+      button.login {
+        width: 100%;
+        height: 64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #797979;
+        border-radius: 8px;
+        color: white;
+        font-size: 1.2rem;
+        margin-bottom: 1rem;
+        span {
+          margin-left: .4rem;
+        }
+      }
+    }
+    
   }
 `
 
@@ -34,25 +72,32 @@ export default class Takeover extends React.Component {
   }
 
   state = {
-    isExpanded: false,
+    isExpanded: true,
   }
 
   togglePanel = () => {
-    this.setState(prevState => {
-      console.log(prevState);
-      return {
-        isExpanded: !prevState.isExpanded,
-      }
-    })
+    this.setState(prevState => ({isExpanded: !prevState.isExpanded}))
   }
 
   render () {
-    console.log(this.state.isExpanded);
+    const { isExpanded } = this.state;
     return (
-      <TakeoverWrapper className={this.state.isExpanded ? 'isExpanded' : ''}>
-        <button onClick={() => this.togglePanel()}>
-          take over this card
+      <TakeoverWrapper className={isExpanded ? 'isExpanded' : ''}>
+        <button className="toggle" onClick={this.togglePanel}>
+          {isExpanded ? 'back to profile' : 'take over this card'}
         </button>
+
+        <div>
+          <p>Please login to see card history and to be able to reset the card and enable super powers!!</p>
+          <section>
+            <button className="login">
+              login with <span className="icon-github"></span>
+            </button>
+            <button className="login">
+              login with <span className="icon-linkedin"></span>
+            </button>
+          </section>
+        </div>
       </TakeoverWrapper>
     )
   }
