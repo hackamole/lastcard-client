6ba0e6bc608cb4f0cd7005fbe0ca0eba5a1b2937
Miguel Carvalho <miguel@brpx.com>
2019-03-21 23:01:58 +0000

    Takeover toggle expand on click

--- components/takeover.js
+++ components/takeover.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
-const Takeover = styled.section`
+const TakeoverWrapper = styled.section`
+  position: fixed;
+  bottom: 0;
   height: 64px;
   width: 100%;
   background-color: #E8E8E8;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   color: #797979;
+  transition: height 500ms cubic-bezier(1, 0, 0, 1);
+  
+  &.isExpanded {
+    height: 100%;
+  }
   
   button {
     width: 100%;
@@ -18,14 +25,36 @@ const Takeover = styled.section`
   }
 `
 
-const toggleLogin = () => {
-  console.log('test');
+export default class Takeover extends React.Component {
+  static async getInitialProps () {
+    // eslint-disable-next-line no-undef
+    // const res = await fetch(api.cards)
+    // const json = await res.json()
+    // return { user: json.stargazers_count }
+  }
+
+  state = {
+    isExpanded: false,
+  }
+
+  togglePanel = () => {
+    this.setState(prevState => {
+      console.log(prevState);
+      return {
+        isExpanded: !prevState.isExpanded,
+      }
+    })
+  }
+
+  render () {
+    console.log(this.state.isExpanded);
+    return (
+      <TakeoverWrapper className={this.state.isExpanded ? 'isExpanded' : ''}>
+        <button onClick={() => this.togglePanel()}>
+          take over this card
+        </button>
+      </TakeoverWrapper>
+    )
+  }
 }
 
-export default () => (
-  <Takeover>
-    <button onClick={() => toggleLogin()}>
-      take over this card
-    </button>
-  </Takeover>
-)
