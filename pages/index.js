d2e2c38757e889dfec9111e9b98234d671b72357
Miguel Carvalho <miguel@brpx.com>
2019-03-22 17:32:53 +0000

    Add about page and new font icons

--- pages/index.js
+++ pages/index.js
@@ -29,13 +29,9 @@ const Wrapper = styled.div`
     nav {
         margin-bottom: 2px;
       a {
+        font-weight: 600;
         text-decoration: none;
         color: #6A38BB;
-      }
-      > ul {
-        display: flex;
-        justify-content: space-between;
-        list-style: none;
       }
     }
   }
@@ -69,7 +65,8 @@ const Wrapper = styled.div`
       max-width: calc(100% - 4rem);
       color: #6A38BB;
     }
-    button {
+    a {
+      text-decoration: none;
       position: absolute;
       bottom: 32px;
       width: calc(100vw - 4rem);
@@ -85,13 +82,9 @@ const Wrapper = styled.div`
 export default () => (
   <Wrapper>
     <header>
-      <Logo />
+      <Link href='/' as='/'><a><Logo /></a></Link>
       <nav>
         <Link href='/login' as='/login'><a>login</a></Link>
-        {/*<ul>*/}
-          {/*<li><Link href='/card' as='/card'><a>card page</a></Link></li>*/}
-          {/*<li><Link href='/profile' as='/profile'><a>profile page</a></Link></li>*/}
-        {/*</ul>*/}
       </nav>
     </header>
 
@@ -100,8 +93,8 @@ export default () => (
       <h1>
         The <span>real</span> professional network
       </h1>
-      <h2>For professional looking to connect to real people that they really know.</h2>
-      <button>Get to know more!</button>
+      <h2>For professional looking to connect with people that they really know.</h2>
+      <Link href='/about' as='/about'><a>Get to know more!</a></Link>
     </div>
   </Wrapper>
 )
