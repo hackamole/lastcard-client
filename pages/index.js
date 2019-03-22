eeddef9881dcfbe6b61d0c067976da99c975a147
Miguel Carvalho <miguel@brpx.com>
2019-03-22 01:44:39 +0000

    Add links for each page on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 export default () => (
-  <div>
+  <Wrapper>
     <Link href='/card' as='/card'><a>card page</a></Link>
     <Link href='/profile' as='/profile'><a>profile page</a></Link>
-  </div>
+  </Wrapper>
 )
