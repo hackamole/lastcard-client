11d78cedf09b187cc3fa5d2d91693825231401ee
Miguel Carvalho <miguel@brpx.com>
2019-03-23 10:15:13 +0000

    Add mock cards/user data

--- pages/profile.js
+++ pages/profile.js
@@ -31,14 +31,97 @@ export default class Profile extends React.Component {
     // const usersRes = await fetch('http://localhost:8000/users/');
     // const users = await usersRes.json();
     // return { cards, users };
-    return {};
+    return {
+      cards: [
+        {
+          id: 1,
+          original_user: 1,
+          url: 'url',
+          qr_code_url: 'url',
+          created_at: 'timestamp',
+          modified_at: 'timestamp',
+          current_user: {
+            id: 1,
+            first_name: 'Celso',
+            last_name: 'Martinho',
+            mobile: '912 345 678',
+            email: 'you@example.com',
+            address: {
+              street: 'string',
+              postcode: 'string',
+              city: 'string',
+              country: 'string',
+            },
+            birthday: 'date',
+            url: 'url',
+            social_profile: 'url'
+          },
+          previousUser: {
+            id: 1,
+            first_name: 'Celso',
+            last_name: 'Martinho',
+            mobile: '912 345 678',
+            email: 'you@example.com',
+            address: {
+              street: 'string',
+              postcode: 'string',
+              city: 'string',
+              country: 'string',
+            },
+            birthday: 'date',
+            url: 'url',
+            social_profile: 'url'
+          },
+      },
+        {
+          id: 2,
+          original_user: 2,
+          url: 'url',
+          qr_code_url: 'url',
+          created_at: 'timestamp',
+          modified_at: 'timestamp',
+          current_user: {
+            id: 1,
+            first_name: 'Celso',
+            last_name: 'Martinho',
+            mobile: '912 345 678',
+            email: 'you@example.com',
+            address: {
+              street: 'string',
+              postcode: 'string',
+              city: 'string',
+              country: 'string',
+            },
+            birthday: 'date',
+            url: 'url',
+            social_profile: 'url'
+          },
+          previousUser: {
+            id: 2,
+            first_name: 'João',
+            last_name: 'Sil',
+            mobile: '912 345 678',
+            email: 'you@example.com',
+            address: {
+              street: 'string',
+              postcode: 'string',
+              city: 'string',
+              country: 'string',
+            },
+            birthday: 'date',
+            url: 'url',
+            social_profile: 'url'
+          },
+        },
+      ]
+    };
   }
 
   render() {
     return (
       <Page showLogo showNav>
         <ProfileWrapper>
-           <DynamicProfileWithLoading user={this.props.user} />
+           <DynamicProfileWithLoading user={this.props.cards.current_user} />
            <DynamicCardsWithLoading cards={this.props.cards} />
         </ProfileWrapper>
       </Page>
