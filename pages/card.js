48dace1e8c5ba9df087c8a47bb43bd49a22a603f
Miguel Carvalho <miguel@brpx.com>
2019-03-23 12:21:19 +0000

    stuff

--- pages/card.js
+++ pages/card.js
@@ -23,13 +23,14 @@ const DynamicComponentWithLoading = dynamic(() => import('../components/profile'
 
 export default class Card extends React.Component {
   static async getInitialProps({ query }) {
-    // const cookies = new Cookies();
+    const cookies = new Cookies();
 
-    // console.log('DEBUG', query);
+    console.log('DEBUG', query);
+    console.log('DEBUG', cookies);
     // console.log('DEBUG', cookies.get('last-card-auth'));
 
     // eslint-disable-next-line no-undef
-    // const cardRes = await fetch(`http://localhost:8000/cards/${query.id}`,
+    // const cardRes = await fetch(`http://lastcard.sytes.net/cards/${query.id}`,
     // {
     //   headers: {
     //     'Authorization': `Token ${cookies.get('last-card-auth')}`,
@@ -38,27 +39,12 @@ export default class Card extends React.Component {
     // const card = await cardRes.json();
     //
     // return { user: card.current_user }
-    return {
-      user: {
-        id: 1,
-        first_name: 'Celso',
-        last_name: 'Martinho',
-        mobile: '912 345 678',
-        email: 'you@example.com',
-        address: {
-          street: 'string',
-          postcode: 'string',
-          city: 'string',
-          country: 'string',
-        },
-        birthday: 'date',
-        url: 'url',
-        social_profile: 'url'
-      }
-    }
   }
 
   render () {
+
+
+    console.log('y');
     return (
       <CardWrapper>
         <DynamicComponentWithLoading user={this.props.user} />
